6feb67482376ad2da64a143387265dc37e41de3f
Janet Pech <jpech06@gmail.com>
2026-02-28 22:10:12 -0600

    comentario

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # Crear una ruta en Render llamada tarea-0. Casos a evaluar: 1.
 # Dicha ruta será accesible por un método GET.
 # Como respuesta, dicha ruta debe regresar lo siguiente (copialo y pégalo):
-# {"respuesta": "Primer tarea realizada"}
+# {"respuesta": "Primer tarea realizada"} 
+# https://github.com/janetgpe/Tarea_0.git cuando ya se sube a github
 
 from fastapi import FastAPI
 
